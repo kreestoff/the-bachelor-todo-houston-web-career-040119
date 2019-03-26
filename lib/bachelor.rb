fb3406c6c9eb6127dccb498eaf4d51a21b01ff89
Christopher McIntosh <christopher.l.mcintosh@outlook.com>
2019-03-26 16:37:52 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,13 +1,39 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  winner = nil 
+  data.each do |season_hash, array|
+    if season_hash == season 
+      array.each do |contestant|
+        if contestant["status"] == "Winner"
+          winner = contestant["name"].split(" ")[0]
+        end
+      end
+    end 
+  end
+  winner
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  answer = nil 
+  data.each do |season_hash, array|
+    array.each do |contestant|
+      if contestant["occupation"] == occupation
+        answer = contestant["name"]
+      end
+    end
+  end
+  answer
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = []
+  data.each do |season_hash, array|
+    array.each do |contestant|
+      if contestant["hometown"] == hometown
+        counter += 1 
+      end
+    end
+  end
+  counter
 end
 
 def get_occupation(data, hometown)
